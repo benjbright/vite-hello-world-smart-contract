21baf4d302792d1cf26bd7e803f6559fe5936111
Ben <105577567+benjbright@users.noreply.github.com>
2022-10-18 14:54:19 +0100

    Update interact.jsx

--- src/utils/interact.jsx
+++ src/utils/interact.jsx
@@ -44,9 +44,8 @@ export const connectWallet = async () => {
       status: (
         <span>
           {" "}
-          Please install
-          <a href="https://metamask.io/download">Metamask</a>, a virtual
-          ethereum wallet, in your browser.
+          Please install <a href="https://metamask.io/download">Metamask</a>, a
+          virtual ethereum wallet, in your browser.
         </span>
       ),
     }
