826158ebaffedef5d69d41980ee3d385bc558527
Ben <105577567+benjbright@users.noreply.github.com>
2022-11-01 14:40:02 +0000

    Update interact.jsx

--- src/utils/interact.jsx
+++ src/utils/interact.jsx
@@ -2,9 +2,12 @@ import contractAbi from "./contract-abi.json"
 import { ethers } from "ethers"
 
 const VITE_API_KEY = import.meta.env.VITE_API_KEY
+const VITE_URL = import.meta.env.VITE_URL
 const contractAddress = "0x83f4E84354AB2FaDfe464c8d24871A11d1832201"
 
-const provider = new ethers.providers.AlchemyProvider("goerli", VITE_API_KEY)
+// const provider = new ethers.providers.AlchemyProvider("goerli", VITE_API_KEY)
+
+const provider = new ethers.providers.JsonRpcProvider(VITE_URL)
 
 export const helloWorldContract = new ethers.Contract(
   contractAddress,
